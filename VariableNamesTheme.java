b175a60d1209ec10061b39b6a0096f67505cebd0
Paul <zalemanpaul@gmail.com>
2025-08-10 12:13:11 +0500

    1. РАЗНЫЕ ПЕРЕМЕННЫЕ

--- VariableNamesTheme.java
+++ VariableNamesTheme.java
@@ -1,5 +1,117 @@
 public class VariableNamesTheme {
     public static void main(String[] args) {
-        
+        System.out.println("1. РАЗНЫЕ ПЕРЕМЕННЫЕ");
+
+        // цифра
+        int digit = 5;
+        System.out.println("Цифра: " + digit);
+
+        // сумма чисел
+        int numberSum = 5 + 8;
+        System.out.println("Сумма чисел: " + numberSum);
+
+        // произведение цифр
+        int multiplyDigits = 5 * 5;
+        System.out.println("Произведение цифр: " + multiplyDigits);
+
+        // максимальное число
+        int maxNumber = 2147483647;
+        System.out.println("Максимальное число: " + maxNumber);
+
+        // количество десятков
+        int tenCount = 4;
+        System.out.println("Количество десятков: " + tenCount);
+
+        // вес собаки
+        float dogWeightKg = 8.3f;
+        System.out.println("Вес собаки: " + dogWeightKg);
+
+        // исходное число
+        int originalNumber = 99;
+        System.out.println("Исходное число: " + originalNumber);
+
+        // процент по вкладу
+        double depositPercent = 2.5;
+        System.out.println("Процент по вкладу: " + depositPercent);
+
+        // символ &
+        char ampersand = '&';
+        System.out.println("Символ &: " + ampersand);
+
+        // код ошибки
+        int errorCode = 400;
+        System.out.println("Код ошибки: " + errorCode);
+
+        // тип сообщения
+        String messageType = "Error";
+        System.out.println("Тип сообщения: " + messageType);
+
+        // число нулей
+        int zeroCount = 4;
+        System.out.println("Число нулей: " + zeroCount);
+
+        // уникальное число
+        long uniqueNumber = 123456789L;
+        System.out.println("Уникальное число: " + uniqueNumber);
+
+        // случайное число
+        int randomNumber = 100;
+        System.out.println("Случайное число: " + randomNumber);
+
+        // математическое выражение
+        String mathExpression = "25 / (1 + 12 / 3)";
+        System.out.println("Математическое выражение: " + mathExpression);
+
+        // счет в игре
+        int gameScore = 15;
+        System.out.println("Счет в игре: " + gameScore);
+
+        // максимальная длина
+        int maxLength = 600;
+        System.out.println("Максимальная длина: " + maxLength);
+
+        // пункт меню
+        String menuItem = "File";
+        System.out.println("Пункт меню: " + menuItem);
+
+        // стоимость кофе
+        double coffeePriceRub = 200;
+        System.out.println("Стоимость кофе: " + coffeePriceRub + " руб");
+
+        // начальная дата
+        String startDate = "04-05-2025";
+        System.out.println("Начальная дата: " + startDate);
+
+        // окончание диапазона
+        String rangeEnd = "1000";
+        System.out.println("Окончание диапазона: " + rangeEnd);
+
+        // имя работника месяца
+        String employeeMonth = "Иван Иванов";
+        System.out.println("Работник месяца: " + employeeMonth);
+
+        // название электронной книги
+        String ebookTitle = "Java. Полное руководство";
+        System.out.println("Электронная книга: " + ebookTitle);
+
+        // размер
+        String size = "XL";
+        System.out.println("Размер: " + size);
+
+        // вместимость
+        int capacity = 100;
+        System.out.println("Вместимость: " + capacity);
+
+        // счетчик
+        int counter = 0;
+        System.out.println("Счетчик: " + counter);
+
+        // путь до файла
+        String filePath = "C:\\Tools\\cmder\\vendor\\init.bat";
+        System.out.println("Путь до файла: " + filePath);
+
+        // количество чисел в строке
+        int numbersInLine = 47;
+        System.out.println("Количество чисел в строке: " + numbersInLine);
     }
 }

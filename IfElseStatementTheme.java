6b8f4a63f3401b84a443955808522d9a9ce17ee3
Paul <zalemanpaul@gmail.com>
2025-08-17 11:35:29 +0500

    Ветвление: if-else (1-5)

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -37,5 +37,72 @@ public class IfElseStatementTheme {
 
         double averageStepCount = (yesterdayStepCount + todayStepCount) / 2.0;
         System.out.printf("Cреднее %.2f", averageStepCount);
+
+        System.out.println("\n\n3. ПРОВЕРКА КОЛИЧЕСТВА ГОСТЕЙ");
+        int guestCount = 1;
+        if (guestCount == 0) {
+            System.out.println("Пока никто не записался на мероприятие!");
+        } else if (guestCount % 2 == 0 && guestCount > 0) {
+            System.out.printf("Записалось %d гостей. Можно формировать пары для конкурсов.", guestCount);
+        } else if (guestCount % 2 != 0 && guestCount > 0) {
+            System.out.printf("Записалось %d гостей. Нужны индивидуальные задания.", guestCount);
+        } else {
+            System.out.println("Ошибка!");
+        }
+
+        System.out.println("\n\n4. ОПРЕДЕЛЕНИЕ ПЕРВОГО СИМВОЛА НИКНЕЙМА");
+        String nickname = "@Dog";
+        char firstChar = nickname.charAt(0);
+
+        // с помощью проверки символа на его вхождение в диапазон кодов
+        if (firstChar >= 'a' && firstChar <= 'z') {
+            System.out.printf("Имя %s начинается с маленькой буквы '%c'%n", nickname, firstChar);
+        } else if (firstChar >= 'A' && firstChar <= 'Z') {
+            System.out.printf("Имя %s начинается с большой буквы '%c'%n", nickname, firstChar);
+        } else if (firstChar >= '0' && firstChar <= '9') {
+            System.out.printf("Имя %s начинается с цифры '%c'%n", nickname, firstChar);
+        } else {
+            System.out.printf("Имя %s начинается с символа '%c'%n", nickname, firstChar);
+        }
+
+        // с помощью методов класса Character, определяющих символы
+        if (Character.isLowerCase(firstChar)) {
+            System.out.printf("Имя %s начинается с маленькой буквы '%c'%n", nickname, firstChar);
+        } else if (Character.isUpperCase(firstChar)) {
+            System.out.printf("Имя %s начинается с большой буквы '%c'%n", nickname, firstChar);
+        } else if (Character.isDigit(firstChar)) {
+            System.out.printf("Имя %s начинается с цифры '%c'%n", nickname, firstChar);
+        } else {
+            System.out.printf("Имя %s начинается с символа '%c'%n", nickname, firstChar);
+        }
+
+        System.out.println("\n5. ИНВЕНТАРИЗАЦИЯ");
+        int dbSerialNumber = 123;
+        int compNumber = 121;
+        if (dbSerialNumber == compNumber) {
+            System.out.printf("[№%d]: компьютер на 3-м этаже в кабинете 2%n", compNumber);
+        } else {
+            int dbHundreds = dbSerialNumber / 100;
+            int dbTens = (dbSerialNumber / 10) % 10;
+            int dbUnits = dbSerialNumber % 10;
+            int compHundreds = compNumber / 100;
+            int compTens = (compNumber / 10) % 10;
+            int compUnits = compNumber % 10;
+
+            String factNumber =
+                    (dbHundreds == compHundreds ? String.valueOf(compHundreds) : "_") +
+                    (dbTens == compTens ? String.valueOf(compTens) : "_") +
+                    (dbUnits == compUnits ? String.valueOf(compUnits) : "_");
+
+            if (dbHundreds == compHundreds || dbTens == compTens || dbUnits == compUnits) {
+                System.out.println("""
+                    Нет полного совпадения:
+                    База данных: [№%d]
+                    Фактический: [№%s]
+                        """.formatted(dbSerialNumber, factNumber));
+            } else {
+                System.out.printf("[№%d]: оборудование не идентифицировано%n", compNumber);
+            }
+        }
     }
 }

901244f93b8ce492102529b43032b028d0b6705e
Paul <zalemanpaul@gmail.com>
2025-08-23 20:50:03 +0500

    Циклы: for, while, do-while (1)

--- CyclesTheme.java
+++ CyclesTheme.java
@@ -1,8 +1,21 @@
 public class CyclesTheme {
     public static void main(String[] args) {
         System.out.println("1. ВЫВОД ASCII-СИМВОЛОВ");
+        System.out.printf("%s %s %s%n", "DECIMAL", "CHARACTER", "DESCRIPTION");
+        String str = "";
+        for (int i = 33; i <= 47; i++) {
+            if (i % 2 != 0) {
+                System.out.printf("%-1s %-9s %-10s %-20s%n", str, i, (char) i, Character.getName(i));
+            }
+        }
 
-        System.out.println("2. ВЫВОД ГЕОМЕТРИЧЕСКИХ ФИГУР");
+        for (int i = 98; i <= 122; i++) {
+            if (i % 2 == 0) {
+                System.out.printf("%-1s %-9s %-10s %-20s%n", str, i, (char) i, Character.getName(i));
+            }
+        }
+
+        System.out.println("\n2. ВЫВОД ГЕОМЕТРИЧЕСКИХ ФИГУР");
         int rows = 5;
 
         for (int i = 0; i < rows; i++) {
